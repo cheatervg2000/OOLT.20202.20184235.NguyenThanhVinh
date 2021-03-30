2c4eed3511fdf0dc1ed14a100c14c5d7e5e6b8fb
Nguyễn Thành Vinh <nthanhvinh10x@gmail.com>
2021-03-30 14:14:35 +0700

    Add Mydate

---
+++ Lab03/DateTest.java
@@ -0,0 +1,15 @@
+package lab03;
+
+public class DateTest {
+	public static void main(String[] agr) {
+		MyDate time = new MyDate("February/13th/2021");
+		time.print();
+		time = new MyDate(3,24,2000);
+		time.print();
+		time = new MyDate();
+		time.print();
+		time.accept();
+		time.print();
+	}
+
+}
